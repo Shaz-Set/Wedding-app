08da0add5ede5e52a818d54dd2d6a8d6aec7a554
Shaz-Set <yalda.setoodeh@ust-global.com>
2021-08-05 15:41:47 +1000

    created the model class, and simple running main.

--- src/main/java/com/yalda/app/App.java
+++ src/main/java/com/yalda/app/App.java
@@ -1,13 +1,56 @@
 package com.yalda.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        WeddingPackages packageOne = new WeddingPackages(1, 4, "none", "none", "sit down", 160);
+        WeddingPackages packageTwo = new WeddingPackages(2, 5, "none", "none", "sit down", 180);
+        WeddingPackages packageThree = new WeddingPackages(3, 6, "none", "none", "sit down", 200);
+        WeddingPackages packageFour = new WeddingPackages(4, 4, "none", "none", "Roving", 120);
+        WeddingPackages packageFive = new WeddingPackages(5, 5, "none", "none", "Roving", 140);
+        WeddingPackages packageSix = new WeddingPackages(6, 6, "none", "none", "Roving", 160);
+
+        List<WeddingPackages> packages = new ArrayList<WeddingPackages>();
+        packages.add(packageOne);
+        packages.add(packageTwo);
+        packages.add(packageThree);
+        packages.add(packageFour);
+        packages.add(packageFive);
+        packages.add(packageSix);
+
+        for (int i = 0; i<packages.size(); i++){
+            System.out.println(packages.get(i).toString());
+        }
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please choose between the packages below:");
+
+        switch (input.next()) {
+            case "1":
+                System.out.println("Congratulations, you chose package One.");
+                break;
+            case "2":
+                System.out.println("Congratulations, you chose package Two.");
+                break;
+            case "3":
+                System.out.println("Congratulations, you chose package Three.");
+                break;
+            case "4":
+                System.out.println("Congratulations, you chose package Four.");
+                break;
+            case "5":
+                System.out.println("Congratulations, you chose package Five.");
+                break;
+            case "6":
+                System.out.println("Congratulations, you chose package Six.");
+                break;
+            default:
+                System.out.println("Please choose a package to proceed !");
+                break;
+        }
     }
 }
---
+++ src/main/java/com/yalda/app/WeddingPackages.java
@@ -0,0 +1,76 @@
+package com.yalda.app;
+
+public class WeddingPackages {
+    int id;
+    int hours;
+    String addOns;
+    String drinkPackages;
+    String styles; //sited down or roving
+    double price;
+
+    public WeddingPackages(int id, int hours, String addOns, String drinkPackages, String styles, double price) {
+        this.id = id;
+        this.hours = hours;
+        this.addOns = addOns;
+        this.drinkPackages = drinkPackages;
+        this.styles = styles;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public String getAddOns() {
+        return addOns;
+    }
+
+    public String getDrinkPackages() {
+        return drinkPackages;
+    }
+
+    public String getStyles() {
+        return styles;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void setAddOns(String addOns) {
+        this.addOns = addOns;
+    }
+
+    public void setDrinkPackages(String drinkPackages) {
+        this.drinkPackages = drinkPackages;
+    }
+
+    public void setStyles(String styles) {
+        this.styles = styles;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Package number: " + this.id + "\n" +
+                "Hours: " + this.hours + "\n" +
+                "Style: " + this.styles + "\n" +
+                "Price: $" + this.price + "\n";
+    }
+}
